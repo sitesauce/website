19f12cf5150bace11171aa8d13dabf81ea3f6a9d
Miguel Piedrafita <soy@miguelpiedrafita.com>
2020-04-08 13:23:13 +0200

    Add OG tag

---
+++ app/Tags/Og.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Tags;
+
+use Statamic\Tags\Tags;
+
+class Og extends Tags
+{
+
+    /**
+     * The {{ og:image }} tag
+     */
+    public function image() : string
+    {
+        return sprintf('https://og-images.sitesauce.app/**%s**.png?md=1', rawurlencode($this->params->get('title')));
+    }
+}
