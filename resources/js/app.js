2e6d2bae875d18c28784fe9f35bcfbfef983e5a0
Miguel Piedrafita <soy@miguelpiedrafita.com>
2020-05-01 11:15:33 +0200

    Move to adblock-ready analytics

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,7 +2,6 @@ import 'alpinejs'
 
 import './components/backend-animation'
 import './components/animations'
-import './components/analytics'
 import './components/dates'
 import './components/chat'
 import './components/producthunt'
