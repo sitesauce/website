345298f617347645c2db525c7d6f5d1044d70bce
Miguel Piedrafita <git@miguelpiedrafita.com>
2020-09-28 10:27:11 +0100

    wip

--- resources/js/components/chat.js
+++ resources/js/components/chat.js
@@ -1,16 +1,1 @@
-window.intercomSettings = {
-	app_id: 'jy3c9b9t',
-}
-
-window.addEventListener(
-	'scroll',
-	() =>
-		setTimeout(() => {
-			let chat = document.createElement('script')
-			chat.src = 'https://widget.intercom.io/widget/jy3c9b9t'
-			document.body.appendChild(chat)
-		}, 1000),
-	{
-		once: true,
-	},
-)
+//
