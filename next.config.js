1c38710e862f92788491f886fdd30c5bbb6525f1
Miguel Piedrafita <soy@miguelpiedrafita.com>
2020-12-16 19:29:32 +0100

    wip

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const withMDX = require('@next/mdx')({
 module.exports = withMDX({
 	pageExtensions: ['js', 'jsx', 'mdx'],
 	poweredByHeader: false,
-	crossOrigin: false,
+	crossOrigin: 'use-credentials',
 	images: {
 		domains: ['brand-assets.sitesauce.app', 'images.sitesauce.app', 'miguelpiedrafita.com'],
 	},
