aaa9f10dc4b59131fe352d1002d53bb05f253e47
Miguel Piedrafita <soy@miguelpiedrafita.com>
2020-12-16 18:58:50 +0100

    wip

--- src/components/Landing/Demo.js
+++ src/components/Landing/Demo.js
@@ -9,7 +9,7 @@ const Demo = () => {
 				<p className="font-ttnorms text-black leading-7 text-xl text-center mb-4 mt-0 max-w-3xl mx-auto">Learn everything Sitesauce has to offer in this short introductory video.</p>
 				<div className="relative mt-20 border-4 border-brand-400 rounded-lg shadow-brand overflow-hidden z-1">
 					<div className="relative p-0" style={{ paddingTop: '56.25% !important', margin: '-2px' }}>
-						<iframe className="absolute inset-0 w-full h-full" title="Demo video" src="https://player.vimeo.com/video/413646262?title=0&byline=0&portrait=0&texttrack=en&dnt=1&transparent=1&quality=1080p" frameBorder="0" allow="autoplay; fullscreen" allowFullScreen />
+						<iframe className="absolute inset-0 w-full h-full" title="Demo video" src="https://player.vimeo.com/video/413646262?title=0&byline=0&portrait=0&texttrack=en&dnt=1&transparent=1&quality=1080p" srcDoc="<style>*{padding:0;margin:0;overflow:hidden}html,body,img{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:0 auto}span{margin:auto;height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style><a href=https://player.vimeo.com/video/413646262?title=0&byline=0&portrait=0&texttrack=en&dnt=1&transparent=1&quality=1080p><img src=https://brand-assets.sitesauce.app/twitter-card.png alt='Sitesauce Demo Video'><span>▶</span></a>" frameBorder="0" allow="autoplay; fullscreen" allowFullScreen loading="lazy" />
 					</div>
 				</div>
 			</div>
