d1deef28a1041252c8a5c395f8fc018abdd25041
Miguel Piedrafita <soy@miguelpiedrafita.com>
2021-01-11 12:30:42 +0100

    wip

--- next.config.js
+++ next.config.js
@@ -35,6 +35,13 @@ module.exports = withMDX({
 			}
 		}
 
+		if (!dev && !isServer) {
+			Object.assign(config.resolve.alias, {
+				react: 'preact/compat',
+				'react-dom': 'preact/compat',
+			})
+		}
+
 		return config
 	},
 	exportPathMap(defaultPathMap, { dev, outDir }) {
